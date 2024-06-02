561d91cc712030f6a22681a28d3b51a581a0afa6
Nikot11 <nikoladavkovski@gmail.com>
2024-06-03 00:10:15 +0200

    Add files via upload

---
+++ Domasno8/index2.js
@@ -0,0 +1,51 @@
+fetch("https://rickandmortyapi.com/api/character")
+.then(response => response.json())
+.then(characters => { 
+    renderCharacters(characters.results)
+})
+
+const cardsContainer = document.querySelector('#cards-container'); 
+
+function renderCharacters(characters) {
+characters.forEach(character => {
+
+  const div = document.createElement('div');
+  const image = document.createElement('img');
+  const name = document.createElement('h3');
+  const species = document.createElement('h3');
+  const status = document.createElement("h3")
+  const gender = document.createElement("h3")
+  const numberOfEpisodes = document.createElement("h3")
+
+
+  div.classList = 'card'
+  image.classList = 'card-img'
+  status.classList = "status"
+  
+
+  image.src = character.image
+  name.innerText = character.name
+  species.innerText = character.species
+  status.innerText = character.status
+  gender.innerText = character.gender
+  numberOfEpisodes.innerHTML = character.episode.length
+
+
+  if(character.status === "Alive") {
+    div.style.backgroundColor = "green"
+  } else if (character.status === "Dead") {
+    div.style.backgroundColor = "red"
+  } else  {
+    div.style.backgroundColor = "gray"
+  }
+  
+
+  div.appendChild(image)
+  div.appendChild(name)
+  div.appendChild(species)
+  div.appendChild(status)
+  div.appendChild(gender)
+  div.appendChild(numberOfEpisodes)
+  cardsContainer.appendChild(div)
+});
+};
