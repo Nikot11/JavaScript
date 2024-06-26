618e335ed78f7233b662ef174c768c1b83b77635
Nikot11 <nikoladavkovski@gmail.com>
2024-06-26 19:23:29 +0200

    Update index.js

--- Node/Domasno 4/index.js
+++ Node/Domasno 4/index.js
@@ -1,1 +1,33 @@
+const http = require("http")
+
+function handler(req, res) {
+    const url = req.url
+
+    const [_, operacija, numOne, numTwo] = req.url.split("/")
+
+    let result
+    switch(operacija) {
+        case "sobiranje":
+            result = Number(numOne) + Number(numTwo)
+              res.end(`${result}`)
+            break 
+        case "razlika": 
+            result = Number(numOne) - Number(numTwo)
+              res.end(`${result}`)
+            break
+        case "mnozenje":
+          result = Number(numOne) * Number(numTwo)
+              res.end(`${result}`)
+            break
+        case "delenje":
+          result = Number(numOne) / Number(numTwo)
+              res.end(`${result}`)
+            break            
+        default: 
+            res.end("Welcome to the calculator app")      
+    }
+}
+
+// const server = http.createServer(handler)
+// server.listen(10001);
 
